6b1b4f166df956c044210041d001f41847458783
Jin <439661734@qq.com>
2022-12-10 18:00:26 +0800

    feat: add lint

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
 export default {
-    // 填入你的session token
-    chatGPTSessionToken: '',
-    // 设置获取消息的重试次数
-    retryTimes: 3,
-    // 在群组中设置唤醒微信机器人的关键词
-    groupKey: 'hello',
-    // 在私聊中设置唤醒微信机器人的关键词
-    privateKey: 'hello',
-    // 重置上下文的关键词，如可设置为reset
-    resetKey: 'reset',
-    // 开启会后收到ChatGPT的自动回复
-    autoReply: true,
-    // 根据正则匹配是否自动通过好友验证
-    friendShipRule: /chatgpt|chat/,
-}
+  // 填入你的session token
+  chatGPTSessionToken: '',
+  // 设置获取消息的重试次数
+  retryTimes: 3,
+  // 在群组中设置唤醒微信机器人的关键词
+  groupKey: 'hello',
+  // 在私聊中设置唤醒微信机器人的关键词
+  privateKey: 'hello',
+  // 重置上下文的关键词，如可设置为reset
+  resetKey: 'reset',
+  // 开启会后收到ChatGPT的自动回复
+  autoReply: true,
+  // 根据正则匹配是否自动通过好友验证
+  friendShipRule: /chatgpt|chat/,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,108 @@
-import {WechatyBuilder} from 'wechaty'
-import qrcodeTerminal from 'qrcode-terminal'
-import config from './config'
-import {replyMessage} from './chatgpt'
+import { WechatyBuilder } from 'wechaty';
+import qrcodeTerminal from 'qrcode-terminal';
+import config from './config';
+import { replyMessage } from './chatgpt';
 
 const pattern = RegExp('@.*' + config.groupKey + ' ');
 
 async function onMessage(msg) {
-    const contact = msg.talker();
-    const contactId = contact.id;
-    const receiver = msg.to();
-    const content = msg.text();
-    const room = msg.room();
-    const alias = await contact.alias() || await contact.name();
-    const isText = msg.type() === bot.Message.Type.Text;
-    if (msg.self()) {
-        return;
-    }
+  const contact = msg.talker();
+  const contactId = contact.id;
+  const receiver = msg.to();
+  const content = msg.text();
+  const room = msg.room();
+  const alias = (await contact.alias()) || (await contact.name());
+  const isText = msg.type() === bot.Message.Type.Text;
+  if (msg.self()) {
+    return;
+  }
 
-    if (room && isText) {
-        const topic = await room.topic();
-        console.log(`Group name: ${topic} talker: ${await contact.name()} content: ${content}`);
-        if (await msg.mentionSelf() && pattern.test(content)) {
-            const groupContent = content.replace(pattern, "");
-            if (groupContent) {
-                // console.log(`Asking content: ${groupContent}`);
-                replyMessage(room, groupContent.trim(), contactId);
-            } else {
-                console.log('Content is empty');
-            }
-        } else {
-            console.log('Content is not within the scope of the customizition format');
-        }
-    } else if (isText) {
-        console.log(`talker: ${alias} content: ${content}`);
-        if (config.autoReply) {
-            if (content.startsWith(config.privateKey)) {
-                // console.log(`Asking content: ${content.substring(config.CustomParameter.length)}`);
-                replyMessage(contact, content.substring(config.privateKey.length).trim(), contactId);
-            } else {
-                console.log('Content is not within the scope of the customizition format');
-            }
-        }
+  if (room && isText) {
+    const topic = await room.topic();
+    console.log(
+      `Group name: ${topic} talker: ${await contact.name()} content: ${content}`
+    );
+    if ((await msg.mentionSelf()) && pattern.test(content)) {
+      const groupContent = content.replace(pattern, '');
+      if (groupContent) {
+        // console.log(`Asking content: ${groupContent}`);
+        replyMessage(room, groupContent.trim(), contactId);
+      } else {
+        console.log('Content is empty');
+      }
+    } else {
+      console.log(
+        'Content is not within the scope of the customizition format'
+      );
     }
+  } else if (isText) {
+    console.log(`talker: ${alias} content: ${content}`);
+    if (config.autoReply) {
+      if (content.startsWith(config.privateKey)) {
+        // console.log(`Asking content: ${content.substring(config.CustomParameter.length)}`);
+        replyMessage(
+          contact,
+          content.substring(config.privateKey.length).trim(),
+          contactId
+        );
+      } else {
+        console.log(
+          'Content is not within the scope of the customizition format'
+        );
+      }
+    }
+  }
 }
 
-
 function onScan(qrcode) {
-    qrcodeTerminal.generate(qrcode); // 在console端显示二维码
-    const qrcodeImageUrl = [
-        'https://api.qrserver.com/v1/create-qr-code/?data=',
-        encodeURIComponent(qrcode),
-    ].join('');
+  qrcodeTerminal.generate(qrcode); // 在console端显示二维码
+  const qrcodeImageUrl = [
+    'https://api.qrserver.com/v1/create-qr-code/?data=',
+    encodeURIComponent(qrcode),
+  ].join('');
 
-    console.log(qrcodeImageUrl);
+  console.log(qrcodeImageUrl);
 }
 
 async function onLogin(user) {
-    console.log(`${user} has logged in`);
-    const date = new Date()
-    console.log(`Current time:${date}`);
-    if (config.autoReply) {
-        console.log(`Automatic robot chat mode has been activated`);
-    }
+  console.log(`${user} has logged in`);
+  const date = new Date();
+  console.log(`Current time:${date}`);
+  if (config.autoReply) {
+    console.log(`Automatic robot chat mode has been activated`);
+  }
 }
 
 function onLogout(user) {
-    console.log(`${user} has logged out`);
+  console.log(`${user} has logged out`);
 }
 
 async function onFriendShip(friendship) {
-    if (friendship.type() === 2) {
-        if (config.friendShipRule.test(friendship.hello())) {
-            await friendship.accept()
-        }
+  if (friendship.type() === 2) {
+    if (config.friendShipRule.test(friendship.hello())) {
+      await friendship.accept();
     }
+  }
 }
 
 const bot = WechatyBuilder.build({
-    name: 'WechatEveryDay',
-    puppet: 'wechaty-puppet-wechat', // 如果有token，记得更换对应的puppet
-    puppetOptions: {
-        uos: true
-    }
-})
-
-bot.on('scan', onScan)
-    .on('login', onLogin)
-    .on('logout', onLogout)
-    .on('message', onMessage)
-if (config.friendShipRule) {
-    bot.on('friendship', onFriendShip);
-}
-
+  name: 'WechatEveryDay',
+  puppet: 'wechaty-puppet-wechat', // 如果有token，记得更换对应的puppet
+  puppetOptions: {
+    uos: true,
+  },
+});
 
 bot
-    .start()
-    .then(() => console.log('Start to log in wechat...'))
-    .catch((e) => console.error(e));
+  .on('scan', onScan)
+  .on('login', onLogin)
+  .on('logout', onLogout)
+  .on('message', onMessage);
+if (config.friendShipRule) {
+  bot.on('friendship', onFriendShip);
+}
 
+bot
+  .start()
+  .then(() => console.log('Start to log in wechat...'))
+  .catch((e) => console.error(e));
