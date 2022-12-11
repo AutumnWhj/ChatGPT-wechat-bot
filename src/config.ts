e7e45be5a086894007c7e7b4f4dd3514f35e7f62
Telepuryang <60610023+TelePuryang@users.noreply.github.com>
2022-12-12 00:39:38 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export default {
   // 在私聊中设置唤醒微信机器人的关键词
   privateKey: '',
   // 重置上下文的关键词，如可设置为reset
-  resetKey: '重置',
+  resetKey: 'reset',
   // 开启会后收到ChatGPT的自动回复
   autoReply: true,
   // 根据正则匹配是否自动通过好友验证
